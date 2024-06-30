08dc0888744f414876348ab83cf8bd9b0188345a
monolab825 <p.smiles777@gmail.com>
2024-06-30 19:19:02 +0700

    clean everything

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
-import createPost from "@/server/actions/create-post";
-import getPosts from "@/server/actions/get-posts";
 
 export default async function Home() {
-  const {error, success} = await getPosts();
-
-  if (error) throw new Error(error);
 
   return (
    <main>
-    {success?.map(post => (
-      <div key={post.id}>
-        <h2>{post.title}</h2>
-      </div>
-    ))}
-    <h1>Welcome to next d JS</h1>
-    <form action={createPost}>
-      <input className="text-black" type="text" name="title" id="title" />
-      <button type="submit">Submit</button>
-    </form>
    </main>
   );
 }
