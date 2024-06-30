16eca0e1cbacb33ad9b2c49bb62d658569a5c3ea
monolab825 <p.smiles777@gmail.com>
2024-06-30 19:54:55 +0700

    install shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
 
   return (
    <main>
+    <h1>Hellow</h1>
+    <Button variant={'destructive'}>Click me</Button>
    </main>
   );
 }
