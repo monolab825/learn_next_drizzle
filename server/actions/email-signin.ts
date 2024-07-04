2cd02090974c12a434320f6e7064d97512ebdd6e
monolab825 <p.smiles777@gmail.com>
2024-07-04 19:03:28 +0700

    verify credentials when login with email

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -5,21 +5,60 @@ import { createSafeActionClient } from "next-safe-action"
 import { db } from ".."
 import { eq } from "drizzle-orm"
 import { users } from "@/server/schema"
+import { generateEmailVerificationToken } from "./tokens"
+import sendVerificationEmail from "./email"
+import { AuthError } from "next-auth"
+import { signIn } from "../auth"
 
 const action = createSafeActionClient()
-
+//Signin via email and password
 const emailSignIn = action
     .schema(loginSchema)
-    .action(async ({parsedInput: {email, password, code}}) => {
-        const existingUser = await db.query.users.findFirst({
-            where: eq(users.email, email)
-        })
+    .action(async ({parsedInput: {email, password}}) => {
+        try {
+            const existingUser = await db.query.users.findFirst({
+                where: eq(users.email, email)
+            })
+    
+            if(existingUser?.email !== email){
+                return {error: "Email not found"}
+            }
+    
+            if(!existingUser?.emailVerified){
+                //Send verification email
+                const verificationToken = await generateEmailVerificationToken(existingUser.email)
+                await sendVerificationEmail(verificationToken[0].email, verificationToken[0].token)
+                return {success: "Email not verified, verification email sent"}
+            }
+    
+            //2FA Authentication TODO
+    
+            //signin
+            await signIn('credentials', {
+                email,
+                password,
+                redirectTo: '/'
+            })
+    
+            return {success: 'berhasil login!'}
+            
+        } catch (error) {
+            //Handle AuthError
+            if(error instanceof AuthError){
+                switch(error.type){
+                    case "CredentialsSignin":
+                        return {error: error.type}
+                    case "AccessDenied":
+                        return {error: error.message}
+                    case 'OAuthSignInError':
+                        return {error: error.message}
+                    default:
+                        return {error: error.message}
+                }
+            }
 
-        if(existingUser?.email !== email){
-            return {error: "Email not found"}
+            throw error;
         }
-
-        return {success: 'berhasil login!'}
     })
 
 export default emailSignIn
--- server/auth.ts
+++ server/auth.ts
@@ -3,6 +3,11 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "@/server"
 import Google from "next-auth/providers/google"
 import Github from "next-auth/providers/github"
+import Credentials from 'next-auth/providers/credentials'
+import { loginSchema } from "@/types/login-schema"
+import { eq } from "drizzle-orm"
+import { users } from "./schema"
+import bcrypt from 'bcrypt'
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET!,
@@ -18,6 +23,30 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         allowDangerousEmailAccountLinking: true,
+    }),
+    Credentials({
+      authorize: async (credentials) => {
+        //vailidate if login safely parse the credentials
+        const validatedFields = loginSchema.safeParse(credentials)
+
+        if(validatedFields.success){
+          const {email, password} = validatedFields.data
+
+          const user = await db.query.users.findFirst({
+            where: eq(users.email, email)
+          })
+          
+          if(!user || !user.password)
+            return null;
+          
+          const passwordMatch = await bcrypt.compare(password, user.password)
+          
+          if(passwordMatch)
+              return user;
+        }
+
+        return null;
+      }
     })
   ],
 })
